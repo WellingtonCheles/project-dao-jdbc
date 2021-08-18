132b1cb324c19227937c668965bd788b35cee438
WellingtonCheles <wellingtoncheles@gmail.com>
2021-08-18 15:28:39 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,26 +10,36 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
-		
+
 		System.out.println("=== TEST 1: seller findById =====");
-		
+
 		Seller seller = sellerDao.findById(3);
-		
+
 		System.out.println(seller);
+
 		
 		System.out.println("\n=== TEST 2: seller findByDepartment =====");
-		
+
 		Department department = new Department(2, null);
-		List<Seller> list = sellerDao.findByDepartment(department); 
-		
+		List<Seller> list = sellerDao.findByDepartment(department);
+
 		for (Seller obj : list) {
-			
+
 			System.out.println(obj);
+		}
+
+		
+		System.out.println("\n=== TEST 3: seller findAll =====");
+
+		list = sellerDao.findAll(); // Reaproveita a lista que já existe no teste 2.
+
+		for (Seller obj : list) {
+
+			System.out.println(obj);
+
 		}
 	}
 
 }
-
-
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -16,149 +16,180 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
-	
+
 	private Connection conn;
-	
+
 	public SellerDaoJDBC(Connection conn) {
-		
+
 		this.conn = conn;
 	}
 
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
-		
+
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
-			
+
 			st = conn.prepareStatement(
-					
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department " 
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ?");
-			
+
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
+
 			st.setInt(1, id);
 			rs = st.executeQuery();
-			 if (rs.next()) {
-				 
-				 Department dep = instanciateDepartment(rs);
-				 Seller obj = instantiateSeller(rs, dep);
-				 return obj;
-				 
-			 }
-			 
-			 return null;
-			
-		}
-		catch (SQLException e) {
-			
+			if (rs.next()) {
+
+				Department dep = instanciateDepartment(rs);
+				Seller obj = instantiateSeller(rs, dep);
+				return obj;
+
+			}
+
+			return null;
+
+		} catch (SQLException e) {
+
 			throw new DbException(e.getMessage());
-		}
-		finally {
-			
+		} finally {
+
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
 	}
 
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
-		 Seller obj = new Seller();
-		 obj.setId(rs.getInt("Id"));
-		 obj.setName(rs.getString("Name"));
-		 obj.setEmail(rs.getString("Email"));
-		 obj.setBaseSalary(rs.getDouble("BaseSalary"));
-		 obj.setBirthDate(rs.getDate("BirthDate"));
-		 obj.setDepartment(dep);
-		 return obj;
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
 	}
 
 	private Department instanciateDepartment(ResultSet rs) throws SQLException {
-		 Department dep = new Department();
-		 dep.setId(rs.getInt("DepartmentId"));
-		 dep.setName(rs.getString("DepName"));
-		 return dep;
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+
+			st = conn.prepareStatement(
+
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "ORDER BY Name");
+
+			rs = st.executeQuery();
+
+			List<Seller> list = new ArrayList<>();
+
+			Map<Integer, Department> map = new HashMap<>(); // Vai guardar qualquer departamento que instanciar.
+
+			while (rs.next()) {
+
+				Department dep = map.get(rs.getInt("DepartmentId")); // Vai verificar se já existe o departamento com o DepartmentId passado.
+																	
+
+				if (dep == null) {
+
+					dep = instanciateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				Seller obj = instantiateSeller(rs, dep); // Instância os vendedores.
+				list.add(obj);
+
+			}
+
+			return list;
+
+		} catch (SQLException e) {
+
+			throw new DbException(e.getMessage());
+		} finally {
+
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
 
 	@Override
 	public List<Seller> findByDepartment(Department department) {
-		
+
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
-			
+
 			st = conn.prepareStatement(
-					
-					"SELECT seller.*,department.Name as DepName " 
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE DepartmentId = ? "
-					+ "ORDER BY Name");
-			
+
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+
 			st.setInt(1, department.getId());
-			
+
 			rs = st.executeQuery();
-			 
+
 			List<Seller> list = new ArrayList<>();
-			
+
 			Map<Integer, Department> map = new HashMap<>(); // Vai guardar qualquer departamento que instanciar.
-			
+
 			while (rs.next()) {
-				
-				
-				 Department dep = map.get(rs.getInt("DepartmentId")); // Vai verificar se já existe o departamento com o DepartmentId passado.
-				 
-				 if (dep == null) {
-					 
-					 dep = instanciateDepartment(rs);
-					 map.put(rs.getInt("DepartmentId"), dep);
-				 }
-				 
-				
-				 Seller obj = instantiateSeller(rs, dep);
-				 list.add(obj);
-				 
-			 }
-			 
-			 return list;
-			
-		}
-		catch (SQLException e) {
-			
+
+				Department dep = map.get(rs.getInt("DepartmentId")); // Vai verificar se já existe o departamento com o
+																		// DepartmentId passado.
+
+				if (dep == null) {
+
+					dep = instanciateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+
+			}
+
+			return list;
+
+		} catch (SQLException e) {
+
 			throw new DbException(e.getMessage());
-		}
-		finally {
-			
+		} finally {
+
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
 	}
-	
 
 }
